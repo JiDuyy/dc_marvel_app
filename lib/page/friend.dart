2f509585d66264a8098be16e228bd5e750ec1b46
DuyKhanhk <duykhanhb60@gmail.com>
2022-11-24 04:47:56 +0700

    Update Page Friend

--- lib/page/friend.dart
+++ lib/page/friend.dart
@@ -1,4 +1,8 @@
+import 'package:dc_marvel_app/components/FrameFriend.dart';
 import 'package:flutter/material.dart';
+import 'package:widget_and_text_animator/widget_and_text_animator.dart';
+
+import '../components/FrameRank.dart';
 
 class Friend extends StatefulWidget {
   const Friend({super.key});
@@ -16,8 +20,118 @@ class _FriendState extends State<Friend> {
       decoration: const BoxDecoration(
         image: DecorationImage(
           image: AssetImage("assets/images/galaxy.gif"),
-          fit: BoxFit.fill,
+          fit: BoxFit.cover,
         ),
+      ),
+      // color: Colors.black,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            flex: 1,
+            child: WidgetAnimator(
+              incomingEffect: WidgetTransitionEffects.incomingSlideInFromTop(),
+              child: Container(
+                color: Colors.orange,
+                child: const Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    'BẠN BÈ',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 1.5,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 10,
+            child: SingleChildScrollView(
+              child: WidgetAnimator(
+                incomingEffect:
+                    WidgetTransitionEffects.incomingSlideInFromLeft(),
+                child: Column(
+                  children: const [
+                    FrameFriend(
+                      frameRank: 'assets/images/FrameNormal.png',
+                      pathAvatar: 'assets/images/IconLevel.png',
+                      userName: "Duyyy",
+                    ),
+                    FrameFriend(
+                      frameRank: 'assets/images/FrameNormal.png',
+                      pathAvatar: 'assets/images/IconLevel.png',
+                      userName: "Duyyyy",
+                    ),
+                    FrameFriend(
+                      frameRank: 'assets/images/FrameNormal.png',
+                      pathAvatar: 'assets/images/IconLevel.png',
+                      userName: "Duyyyyy",
+                    ),
+                    FrameFriend(
+                      frameRank: 'assets/images/FrameNormal.png',
+                      pathAvatar: 'assets/images/IconLevel.png',
+                      userName: "Duyyyyyy",
+                    ),
+                    FrameFriend(
+                      frameRank: 'assets/images/FrameNormal.png',
+                      pathAvatar: 'assets/images/IconLevel.png',
+                      userName: "Duyyyyyyy",
+                    ),
+                    FrameFriend(
+                      frameRank: 'assets/images/FrameNormal.png',
+                      pathAvatar: 'assets/images/IconLevel.png',
+                      userName: "Duyyyyyyyy",
+                    ),
+                    FrameFriend(
+                      frameRank: 'assets/images/FrameNormal.png',
+                      pathAvatar: 'assets/images/IconLevel.png',
+                      userName: "kdy",
+                    ),
+                    FrameFriend(
+                      frameRank: 'assets/images/FrameNormal.png',
+                      pathAvatar: 'assets/images/IconLevel.png',
+                      userName: "kdy",
+                    ),
+                    FrameFriend(
+                      frameRank: 'assets/images/FrameNormal.png',
+                      pathAvatar: 'assets/images/IconLevel.png',
+                      userName: "Duyyyyyyyy",
+                    ),
+                    FrameFriend(
+                      frameRank: 'assets/images/FrameNormal.png',
+                      pathAvatar: 'assets/images/IconLevel.png',
+                      userName: "Duyyyyyyyy",
+                    ),
+                    FrameFriend(
+                      frameRank: 'assets/images/FrameNormal.png',
+                      pathAvatar: 'assets/images/IconLevel.png',
+                      userName: "kdy",
+                    ),
+                    FrameFriend(
+                      frameRank: 'assets/images/FrameNormal.png',
+                      pathAvatar: 'assets/images/IconLevel.png',
+                      userName: "kdy",
+                    ),
+                    FrameFriend(
+                      frameRank: 'assets/images/FrameNormal.png',
+                      pathAvatar: 'assets/images/IconLevel.png',
+                      userName: "kdy",
+                    ),
+                    FrameFriend(
+                      frameRank: 'assets/images/FrameNormal.png',
+                      pathAvatar: 'assets/images/IconLevel.png',
+                      userName: "kdy",
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }

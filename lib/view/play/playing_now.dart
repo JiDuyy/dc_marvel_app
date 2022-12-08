ef9f9c821cf5a1c6be61f9576994d4590775ae4d
DuyKhanhk <duykhanhb60@gmail.com>
2022-12-09 00:46:41 +0700

    5% play now

--- lib/view/play/playing_now.dart
+++ lib/view/play/playing_now.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import '../../components/Answer.dart';
 import '../../components/Icon_helper.dart';
@@ -13,7 +15,8 @@ class PlayingGame extends StatefulWidget {
 }
 
 class _PlayingGameState extends State<PlayingGame> {
-  
+  final auth = FirebaseAuth.instance;
+  final _database = FirebaseDatabase.instance.ref();
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -26,130 +29,160 @@ class _PlayingGameState extends State<PlayingGame> {
             fit: BoxFit.fill,
           ),
         ),
-        child: Column(
-          children: [
-            Expanded(
-              child: Align(
-                alignment: Alignment.centerRight,
-                child: InkWell(
-                  onTap: () {
-                    Navigator.of(context).push(
-                      PageRouteBuilder(
-                        opaque: false,
-                        pageBuilder: (BuildContext context, _, __) =>
-                            const ShowDialogSettingPlayGame(),
-                      ),
-                    );
-                  },
-                  child: Container(
-                    width: size.width / 10,
-                    height: size.width / 10,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/images/icon_setting.png"),
-                        // fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 4,
-              child: Container(
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("assets/images/FrameQuestion.png"),
-                    fit: BoxFit.fill,
-                  ),
-                ),
-                child: Column(
-                  children: [
-                    Expanded(
-                      flex: 6,
-                      child: Padding(
-                        padding: EdgeInsets.all(size.width / 15),
-                        child: const Align(
-                          alignment: Alignment.topLeft,
-                          child: Text(
-                            'Mình tự đánh mình, mình cảm thấy đâu là mình mạnh hay mình yếu?',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.only(right: 12),
-                        child: Align(
-                          alignment: Alignment.centerRight,
-                          child: Text(
-                            '30',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
+        child: StreamBuilder(
+          stream: _database.child('questions/1').onValue,
+          builder: ((context, snapshot) {
+            if (snapshot.hasData && snapshot.data != null) {
+              final data = Map<String, dynamic>.from(
+                Map<String, dynamic>.from((snapshot.data as DatabaseEvent)
+                    .snapshot
+                    .value as Map<dynamic, dynamic>),
+              );
+              return Column(
+                children: [
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            PageRouteBuilder(
+                              opaque: false,
+                              pageBuilder: (BuildContext context, _, __) =>
+                                  const ShowDialogSettingPlayGame(),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          width: size.width / 10,
+                          height: size.width / 10,
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                              image:
+                                  AssetImage("assets/images/icon_setting.png"),
+                              // fit: BoxFit.cover,
                             ),
                           ),
                         ),
                       ),
                     ),
-                    Expanded(
-                      child: Text(
-                        'Chương 1: Vũ trụ',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
+                  ),
+                  Expanded(
+                    flex: 4,
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("assets/images/FrameQuestion.png"),
+                          fit: BoxFit.fill,
                         ),
                       ),
+                      child: Column(
+                        children: [
+                          Expanded(
+                            flex: 6,
+                            child: Padding(
+                              padding: EdgeInsets.all(size.width / 15),
+                              child: Align(
+                                alignment: Alignment.topLeft,
+                                child: Text(
+                                  data['title'].toString(),
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.only(right: 12),
+                              child: Align(
+                                alignment: Alignment.centerRight,
+                                child: Text(
+                                  '30',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Text(
+                              data['chapter'].toString(),
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ],
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 4,
-              child: Column(
-                children: const [
-                  Expanded(
-                    child: Answer(title: 'A', caption: 'Lú cái đầu'),
                   ),
                   Expanded(
-                    child: Answer(title: 'B', caption: 'Lú cái đầu'),
+                    flex: 4,
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: Answer(
+                            title: 'A',
+                            caption: data['1'].toString(),
+                          ),
+                        ),
+                        Expanded(
+                          child: Answer(
+                            title: 'B',
+                            caption: data['2'].toString(),
+                          ),
+                        ),
+                        Expanded(
+                          child: Answer(
+                            title: 'C',
+                            caption: data['3'].toString(),
+                          ),
+                        ),
+                        Expanded(
+                          child: Answer(
+                            title: 'D',
+                            caption: data['4'].toString(),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                   Expanded(
-                    child: Answer(title: 'C', caption: 'Lú cái đầu'),
+                    flex: 2,
+                    child: Container(
+                      margin: const EdgeInsets.only(top: 10),
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("assets/images/FramePlayer.png"),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Icon_helper(
+                              url: 'assets/images/icon_hammer.png', items: 5),
+                          Icon_helper(
+                              url: 'assets/images/icon_spider.png', items: 5),
+                          Icon_helper(
+                              url: 'assets/images/icon_bat.png', items: 5),
+                          Icon_helper(
+                              url: 'assets/images/icons_khien.png', items: 5),
+                        ],
+                      ),
+                    ),
                   ),
-                  Expanded(
-                    child: Answer(title: 'D', caption: 'Lú cái đầu'),
+                  const Spacer(
+                    flex: 1,
                   ),
                 ],
-              ),
-            ),
-            Expanded(
-              flex: 2,
-              child: Container(
-                margin: const EdgeInsets.only(top: 10),
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("assets/images/FramePlayer.png"),
-                    fit: BoxFit.fill,
-                  ),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Icon_helper(url: 'assets/images/icon_hammer.png', items: 5),
-                    Icon_helper(url: 'assets/images/icon_spider.png', items: 5),
-                    Icon_helper(url: 'assets/images/icon_bat.png', items: 5),
-                    Icon_helper(url: 'assets/images/icons_khien.png', items: 5),
-                  ],
-                ),
-              ),
-            ),
-            const Spacer(
-              flex: 1,
-            ),
-          ],
+              );
+            }
+            return CircularProgressIndicator();
+          }),
         ),
       ),
     );

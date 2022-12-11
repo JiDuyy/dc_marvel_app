b37c378cc44a70c3b0599d5854d95af02d1a6eea
Your Name <cong@gmail.com>
2022-12-11 15:44:54 +0700

    avartarL2

--- lib/view/avatar.dart
+++ lib/view/avatar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Avatar extends StatefulWidget {
   const Avatar({super.key});
@@ -10,6 +11,7 @@ class Avatar extends StatefulWidget {
 
 class _AvatarState extends State<Avatar> {
   final _database = FirebaseDatabase.instance.ref();
+  final auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,10 +28,17 @@ class _AvatarState extends State<Avatar> {
                 color: Colors.white.withOpacity(0.9),
                 child: ListView(
                   children: [
-                    Image.asset('assets/images/AvatarChibi1.jpg'),
-                    Image.asset('assets/images/AvatarChibi2.jpg')
+                    InkWell(
+                      onTap: () {
+                        _database
+                            .child('members/${auth.currentUser!.uid}/image')
+                            .set(1);
+                      },
+                      child: Image.asset('assets/images/AvatarChibi1.jpg'),
+                    ),
+                    Image.asset('assets/images/AvatarChibi2.jpg'),
                   ],
-                ),
+                ), 
               ),
             ],
           ),

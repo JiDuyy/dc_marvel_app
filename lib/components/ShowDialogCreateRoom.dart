3bbe0bef1a557dc82bcc95f62c165fd8b4493f0a
JiDuy <jiduy02@gmail.com>
2022-12-20 01:11:29 +0700

    test room l4

--- lib/components/ShowDialogCreateRoom.dart
+++ lib/components/ShowDialogCreateRoom.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, no_leading_underscores_for_local_identifiers
 import 'dart:async';
 import 'package:dc_marvel_app/components/FrameEx.dart';
+import 'package:dc_marvel_app/components/InviteFriend.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
@@ -164,7 +165,6 @@ class _ShowDialogCreateRoomState extends State<ShowDialogCreateRoom> {
           padding: const EdgeInsets.all(10.0),
           decoration: BoxDecoration(
             color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
             image: DecorationImage(
               image: AssetImage("assets/images/FrameTitle.png"),
               fit: BoxFit.cover,
@@ -317,12 +317,26 @@ class _ShowDialogCreateRoomState extends State<ShowDialogCreateRoom> {
                         Stack(
                           alignment: Alignment.topRight,
                           children: [
-                            PlayerRoom(
-                              size: size,
-                              path: userImageTwo.text == ""
-                                  ? 'assets/images/iconAddfriend.png'
-                                  : 'assets/images/AvatarChibi${userImageTwo.text}.jpg',
-                              pathFrameRank: frameRankUserTwo.text,
+                            InkWell(
+                              onTap: () {
+                                Navigator.of(context).push(
+                                  PageRouteBuilder(
+                                    opaque: false,
+                                    pageBuilder:
+                                        (BuildContext context, _, __) =>
+                                            InviteFriend(
+                                      roomId: widget.roomId,
+                                    ),
+                                  ),
+                                );
+                              },
+                              child: PlayerRoom(
+                                size: size,
+                                path: userImageTwo.text == ""
+                                    ? 'assets/images/iconAddfriend.png'
+                                    : 'assets/images/AvatarChibi${userImageTwo.text}.jpg',
+                                pathFrameRank: frameRankUserTwo.text,
+                              ),
                             ),
                             userOne.text == keyUser.text && userTwo.text != ""
                                 ? InkWell(
